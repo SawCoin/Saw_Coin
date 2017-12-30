a4c4dac244378ce2f4109e0ec319525a11c2aad4
Shredder <Shredder@Sebastians-MacBook-Pro.local>
2017-12-30 01:14:48 +0100

    added mining and difficulty

--- subproject/main.js
+++ subproject/main.js
@@ -7,15 +7,26 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.hash = this.calculateHash();
+				this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
+
+		mineBlock(difficulty){
+			while(this.hash.substring(0, difficulty) !==Array(difficulty + 1).join("0")){
+				this.nonce++;
+				this.hash = this.calculateHash();
+			}
+			console.log("Block mined " + this.hash);
+		}
 }
+
 	class Blockchain{
 		constructor() {
          this.chain = [this.createGenesisBlock()];
+				 this.difficulty = 2;
      }
 
      createGenesisBlock() {
@@ -28,13 +39,28 @@ class Block {
 
      addBlock(newBlock) {
          newBlock.previousHash = this.getLatestBlock().hash;
-         newBlock.hash = newBlock.calculateHash();
+      	 newBlock.mineBlock(this.difficulty)
          this.chain.push(newBlock);
      }
+
+		 isChainValid(){
+			 for(let i = 1; i<this.chain.length; i++){
+				 const currentBlock = this.chain[i];
+				 const previousBlock = this.chain[i-1];
+
+				 if(currentBlock.hash !== currentBlock.calculateHash()){
+					 return false;
+				 }
+				 if(currentBlock.previousHash !== previousBlock.hash){
+					 return false;
+				 }
+			 }
+			 return true;
+		 }
 }
 
 let sawCoin = new Blockchain();
+console.log("Mining block 1..");
 sawCoin.addBlock(new Block(1,"10/07/2017", {amount:4}));
-sawCoin.addBlock(new Block(1,"10/07/2017", {amount:10}));
-sawCoin.addBlock(new Block(1,"10/07/2017", {amount:18}));
-console.log(JSON.stringify(sawCoin,null,4));
+console.log("Mining block 2..");
+sawCoin.addBlock(new Block(2,"10/07/2017", {amount:10}));
